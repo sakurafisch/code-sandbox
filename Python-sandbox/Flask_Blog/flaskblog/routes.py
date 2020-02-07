c88da1db3f4d67a8491b50adafc8d25485b635d3
winnerwinter1998 <winnerwinter@126.com>
2020-02-08 00:50:32 +0800

    落实登录功能和退出功能

--- Python-sandbox/Flask_Blog/flaskblog/routes.py
+++ Python-sandbox/Flask_Blog/flaskblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post # 该 import 语句需要运行在 db = SQLAlchemy(app) 之后
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -43,9 +44,15 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
