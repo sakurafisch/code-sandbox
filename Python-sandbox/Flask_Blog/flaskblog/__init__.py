daf1386666aff4693c6898f90c5bb6f0b4956b83
winnerwinter1998 <winnerwinter@126.com>
2020-02-07 23:43:50 +0800

    重新整理了依赖包

--- Python-sandbox/Flask_Blog/flaskblog/__init__.py
+++ Python-sandbox/Flask_Blog/flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '67f3196146d682809d7992a0351e2b26'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
