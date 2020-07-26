27a4634e05eca94e6b972c1d9a2e987253fff276
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-07-26 18:41:47 +0800

    引入静态 js 脚本

--- TryParseFile/main.go
+++ TryParseFile/main.go
@@ -12,6 +12,10 @@ func welcome(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	server := http.Server{Addr: ":8090"}
+
+	// 若 url 以 /static 开头，把请求转发给指定的路径
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", welcome)
 	server.ListenAndServe()
 }
---
+++ TryParseFile/static/js/index.js
@@ -0,0 +1,3 @@
+myclick = function() {
+    alert("那么，你喜欢我吗")
+}
