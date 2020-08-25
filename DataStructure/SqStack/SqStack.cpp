b8b7656cfd5840dfbd67cae4308a85c8bb02b3dc
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-08-25 09:34:10 +0800

    栈的顺序存储结构

---
+++ DataStructure/SqStack/SqStack.cpp
@@ -0,0 +1,40 @@
+#define MaxSize 50
+typedef int ElemType;
+
+typedef struct SqStack
+{
+    ElemType data[MaxSize];
+    int top;
+};
+
+void InitStack(SqStack &S) {
+    S.top = -1;
+}
+
+bool StackEmpty (SqStack S) {
+    if (S.top == -1)
+        return true;
+    else 
+        return false;
+}
+
+bool Push(SqStack &S, ElemType x) {
+    if (S.top == MaxSize - 1)
+        return false;
+    S.data[++S.top] = x;
+    return true;
+}
+
+bool Pop(SqStack &S, ElemType &x) {
+    if (S.top == -1)
+        return false;
+    x = S.data[S.top--];
+    return true;
+}
+
+bool GetTop(SqStack S, ElemType &x) {
+    if (S.top == -1)
+        return false;
+    x = S.data[S.top];
+    return true;
+}
